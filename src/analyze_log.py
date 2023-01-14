f309caa892c7bba54c63213aeea81070a7d24751
mandioquynha <machado.amandag@gmail.com>
2023-01-14 14:54:47 -0300

    feat: :sparkles: implementado módulo análise_log e módulo track_orders concluido
    requisito 2 concluido

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,12 @@
+import csv
+     
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
-"iniciando projeto"
+    if not path_to_file.endswith("csv"):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    try:
+        with open(path_to_file) as file:
+            read = csv.reader(file)
+            return list(read)
+    except:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,56 @@
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+
+    def __init__(self):
+        self.estoque = list()
+
     def __len__(self):
-        pass
+        return len(self.estoque)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.estoque.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        most_requested = dict()
+        for order in self.estoque:
+            if order[0] == customer:
+                if order[1] in most_requested:
+                    most_requested[order[1]] += 1
+                else:
+                    most_requested[order[1]] = 1
+        return max(most_requested, key=most_requested.get)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        disher = set()
+        requests = set()
+        for order in self.estoque:
+            disher.add(order[1])
+            if order[0] == customer:
+                requests.add(order[1])
+        return disher - requests
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        days = set()
+        visited_days = set()
+        for order in self.estoque:
+            days.add(order[2])
+            if order[0] == customer:
+                visited_days.add(order[2])
+        return days - visited_days
 
     def get_busiest_day(self):
-        pass
+        day = dict()
+        for order in self.estoque:
+            if order[2] in day:
+                day[order[2]] += 1
+            else:
+                day[order[2]] = 1
+        return max(day, key=day.get)
 
     def get_least_busy_day(self):
-        pass
+        day = dict()
+        for order in self.estoque:
+            if order[2] not in day:
+                day[order[2]] = 1
+            else:
+                day[order[2]] += 1
+        return min(day, key=day.get)
